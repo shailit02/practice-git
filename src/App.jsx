faea29cc78cea8fa770e1967e6d579cb6d33aa56
Shailit Sharma <shailitsharma@gmail.com>
2025-03-08 00:56:54 +0530

    signup page is created

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Login from "./pages/Login.jsx";
 import About from "./pages/About.jsx";
 import Home from "./pages/Home.jsx";
 import Header from "./components/Header.jsx";
+import SignUp from "./pages/SIgnUp.jsx";
+
+
 
 const App = () => {
   return (
@@ -13,6 +16,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp/>} />
       </Routes>
     </>
   );
