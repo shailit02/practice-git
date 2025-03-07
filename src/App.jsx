8d1fabceb636e621bb6800807b23be554c2fc127
Shubham Rauthan <codexshubham@gmail.com>
2025-03-08 00:10:11 +0530

    Created header

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Login  from './pages/Login.jsx'
-import  About  from './pages/About.jsx'
-import  Home  from './pages/Home.jsx'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login.jsx";
+import About from "./pages/About.jsx";
+import Home from "./pages/Home.jsx";
+import Header from "./components/Header.jsx";
+
 const App = () => {
   return (
     <>
+      <Header />
       <Routes>
-        <Route path='/about' element={ <About/>} />
-        <Route path='/home' element={ <Home/>} />
-        <Route path='/login' element={ <Login/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
