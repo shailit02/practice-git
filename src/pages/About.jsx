466324ac194a614a4d3ef49a002fd3b4cb393923
Shailit Sharma <shailitsharma@gmail.com>
2025-03-08 00:18:22 +0530

    Created about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-      about
+      <h1>About page </h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur est eos, earum accusamus aut quae omnis commodi tempore. Ex dolores, iusto minus deleniti veritatis quod. Laboriosam minus saepe atque consequuntur.</p>
     </div>
   )
 }
