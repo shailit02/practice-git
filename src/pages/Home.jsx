1f683aa43e7faede28739a3f190e8afc18d92a24
Shailit Sharma <shailitsharma@gmail.com>
2025-03-08 00:32:16 +0530

    Created  homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-      home
-      
+      <h1>HOME PAGE IS DONE</h1>
+      <img src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="image is slow " />
+
     </>
   )
 }
